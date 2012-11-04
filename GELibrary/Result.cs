e3c0787b40c939a6c0e793c1942f7f4d5ec8eb52
Billy Vierra <bvierra@cpan.org>
2012-11-03 17:00:44 -0700

    Initial addition of GnomoriaEnhanced c# project Initial Release of GnomoriaEnhanced 0.0.1.0

---
+++ GELibrary/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GELibrary
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Message { get; set; }
+    }
+}
