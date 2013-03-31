62861abb16ee49f45ec82cafa25a123769145100
hautesse <hautesserres@gmail.com>
2013-03-31 18:41:00 +0200

    Internal: - Added a constructor with all attributes to the Result class.

--- GELibrary/Result.cs
+++ GELibrary/Result.cs
@@ -7,8 +7,26 @@ namespace GELibrary
 {
     public class Result
     {
+        /// <summary>
+        /// Default constructor, success = true, ErrorMessage = ""
+        /// </summary>
+        public Result()
+        {
+            Success = true;
+            ErrorMessage = "";
+        }
+        /// <summary>
+        /// Complete constructor, useful to create and return a result in one instruction.
+        /// </summary>
+        /// <param name="pSuccess"></param>
+        /// <param name="pErrorMessage"></param>
+        public Result(bool pSuccess, String pErrorMessage)
+        {
+            Success = pSuccess;
+            ErrorMessage = pErrorMessage;
+        }
+
         public bool Success { get; set; }
         public string ErrorMessage { get; set; }
-        public string Message { get; set; }
     }
 }
